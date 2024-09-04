e08f841ffcb6c1fa737f8c6898bf4da89ed1d1d1
Makfi-i <althafkf1915@gmail.com>
2024-09-04 17:40:25 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ const uid = Uuid();
 
 final theme = ThemeData(
   useMaterial3: true,
-  brightness: Brightness.light,
+  brightness: Brightness.dark,
   textTheme: GoogleFonts.kanitTextTheme(),
   scaffoldBackgroundColor: Colors.white,
   appBarTheme: const AppBarTheme(color: Colors.white),
